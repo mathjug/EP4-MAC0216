f615a88ff74551acb4e4e0941b7db0904fe75c84
CaioGama00 <caio.gama@usp.br>
2021-12-02 11:08:43 -0300

    selfzinho maroto

--- JogodaVelha.py
+++ JogodaVelha.py
@@ -1,98 +1,98 @@
-def menu():
-    c = 1
-    c = int(input("\nSair(0) \n" + "Jogar(1) \n"))
-    if c == 1:
-        game()
-    else:
-        print("Bye Bye...")
-        exit()
-
-def board_reset(board):
-    for i in range(3):
-        for j in range(3):
-            board[i][j] = 0
-
-def game():
-    jogada=0
-
-    board_reset(board)
-
-    while ganhou() == 0:
-        print("\nJogador", jogada%2 + 1)
-        exibe()
-        linha  = int(input("\nDigite a linha de sua jogada: "))
-        coluna = int(input("Digite a coluna de sua jogada: "))
-
-        if board[linha-1][coluna-1] == 0:
-            if(jogada%2+1)==1:
-                board[linha-1][coluna-1]=1
-            else:
-                board[linha-1][coluna-1]=-1
+class TicTacToe:
+    def __init__(self):
+        self.board = [[0,0,0],[0,0,0],[0,0,0]]
+        self.c = 1
+        self.c = int(input("\nSair(0) \n" + "Jogar(1) \n"))
+        if self.c == 1:
+            self.game()
         else:
-            print("\nJa foi preenchido boroca, tente em outro lugar")
-            jogada -=1
+            print("Bye Bye...")
+            exit()
 
-        if ganhou():
-            exibe()
-            print("\nParabens! Jogador",jogada%2 + 1,"ganhou depois de", jogada+1,"rodadas")
-            t = str(input("\nQuer jogar novamente? "))
+    def board_reset(self,board):
+        for i in range(3):
+            for j in range(3):
+                board[i][j] = 0
+
+    def game(self):
+        jogada=0
+
+        self.board_reset(self.board)
+
+        while self.ganhou() == 0:
+            print("\nJogador", jogada%2 + 1)
+            self.exibe()
+            linha  = int(input("\nDigite a linha de sua jogada: "))
+            coluna = int(input("Digite a coluna de sua jogada: "))
+
+            if self.board[linha-1][coluna-1] == 0:
+                if(jogada%2+1)==1:
+                    self.board[linha-1][coluna-1]=1
+                else:
+                    self.board[linha-1][coluna-1]=-1
+            else:
+                print("\nJa foi preenchido boroca, tente em outro lugar")
+                jogada -=1
+
+            if self.ganhou():
+                self.exibe()
+                print("\nParabens! Jogador",jogada%2 + 1,"ganhou depois de", jogada+1,"rodadas")
+                t = str(input("\nQuer jogar novamente? "))
+                if t == "sim":
+                    self.menu()
+                else: 
+                    print("\nObrigado por jogar!\n")
+                    exit()
+            jogada +=1
+        
+    def ganhou(self):
+        #verificar linhas
+        a = 0
+        for i in range(3):
+            soma = self.board[i][0]+self.board[i][1]+self.board[i][2]
+            if self.board[i][0] == 0 or self.board[i][1] == 0 or self.board[i][2] == 0 : a = 1
+            if soma==3 or soma ==-3:
+                return 1
+
+        #verificar colunas
+        for i in range(3):
+            soma = self.board[0][i]+self.board[1][i]+self.board[2][i]
+            if self.board[0][i] == 0 or self.board[1][i] == 0 or self.board[2][i] == 0 : a = 1
+            if soma==3 or soma ==-3:
+                return 1
+
+        if a == 0 :
+            t = str(input("\nDeu Velha! Quer jogar novamente? "))
             if t == "sim":
-                menu()
+                self.menu()
             else: 
                 print("\nObrigado por jogar!\n")
                 exit()
-        jogada +=1
-    
-def ganhou():
-    #verificar linhas
-    a = 0
-    for i in range(3):
-        soma = board[i][0]+board[i][1]+board[i][2]
-        if board[i][0] == 0 or board[i][1] == 0 or board[i][2] == 0 : a = 1
-        if soma==3 or soma ==-3:
+
+        #verificar diagonais
+        diagonal1 = self.board[0][0]+self.board[1][1]+self.board[2][2]
+        diagonal2 = self.board[0][2]+self.board[1][1]+self.board[2][0]
+        if diagonal1==3 or diagonal1==-3 or diagonal2==3 or diagonal2==-3:
             return 1
 
-     #verificar colunas
-    for i in range(3):
-        soma = board[0][i]+board[1][i]+board[2][i]
-        if board[0][i] == 0 or board[1][i] == 0 or board[2][i] == 0 : a = 1
-        if soma==3 or soma ==-3:
-            return 1
+        return 0
 
-    if a == 0 :
-        t = str(input("\nDeu Velha! Quer jogar novamente? "))
-        if t == "sim":
-            menu()
-        else: 
-            print("\nObrigado por jogar!\n")
-            exit()
+    def exibe(self):
+        for i in range(3):
+            print("|", end='')
+            for j in range(3):
+                if self.board[i][j] == 0:
+                    print(" _ |", end='')
+                elif self.board[i][j] == 1:
+                    print(" X |", end='')
+                elif self.board[i][j] == -1:
+                    print(" O |", end='')
 
-    #verificar diagonais
-    diagonal1 = board[0][0]+board[1][1]+board[2][2]
-    diagonal2 = board[0][2]+board[1][1]+board[2][0]
-    if diagonal1==3 or diagonal1==-3 or diagonal2==3 or diagonal2==-3:
-        return 1
-
-    return 0
-
-def exibe():
-    for i in range(3):
-        print("|", end='')
-        for j in range(3):
-            if board[i][j] == 0:
-                print(" _ |", end='')
-            elif board[i][j] == 1:
-                print(" X |", end='')
-            elif board[i][j] == -1:
-                print(" O |", end='')
-
-        print()
+            print()
                 
-board=[[0,0,0],[0,0,0],[0,0,0]]
-
 c = str(input("Quer jogar? "))
 if c != "sim" and c != "nao":
     while c != "sim" and c != "nao":
         c = str(input("Quer jogar? "))
 if c == "sim":
-    menu()
+    Jogo = TicTacToe()
